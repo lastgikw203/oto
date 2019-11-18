eebf159a9c5b96fcc21c49fe84e21625ced08b84
lastgikw203 <55953948+lastgikw203@users.noreply.github.com>
2019-11-18 17:42:22 +0800

    Update crashdetect.sh

--- crashdetect.sh
+++ crashdetect.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while [[ ! $(pidof OtohitsApp) ]]; do
         killall OtohitsApp OtohitsViewer
-        /root/OtohitsApp/OtohitsApp -autostart > /dev/null
+        /root/OtohitsApp/OtohitsApp -nolog -autostart > /dev/null
         exit
 done
